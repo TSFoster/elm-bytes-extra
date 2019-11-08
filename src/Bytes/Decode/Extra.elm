b0a1f0f8b7cd7ee7fa49aee1c89e7529688e95cf
T.S. Foster <hi@tsf.io>
2019-11-08 10:22:23 +0000

    Add extra decode helpers

--- src/Bytes/Decode/Extra.elm
+++ src/Bytes/Decode/Extra.elm
@@ -1,6 +1,6 @@
-module Bytes.Decode.Extra exposing (byteValues, list)
+module Bytes.Decode.Extra exposing (andMap, byteValues, hardcoded, list, map6, map7, map8)
 
-import Bytes.Decode exposing (Decoder, Step(..), loop, map, succeed, unsignedInt8)
+import Bytes.Decode exposing (..)
 
 
 list : Int -> Decoder a -> Decoder (List a)
@@ -20,3 +20,36 @@ listStep elementDecoder ( n, elements ) =
 byteValues : Int -> Decoder (List Int)
 byteValues length =
     list length unsignedInt8
+
+
+required : Decoder a -> Decoder (a -> b) -> Decoder b
+required aDecoder fnDecoder =
+    map2 (<|) fnDecoder aDecoder
+
+
+andMap : Decoder a -> Decoder (a -> b) -> Decoder b
+andMap aDecoder fnDecoder =
+    map2 (<|) fnDecoder aDecoder
+
+
+map6 : (a -> b -> c -> d -> e -> f -> result) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f -> Decoder result
+map6 f decoderA decoderB decoderC decoderD decoderE decoderF =
+    map5 f decoderA decoderB decoderC decoderD decoderE
+        |> andMap decoderF
+
+
+map7 : (a -> b -> c -> d -> e -> f -> g -> result) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f -> Decoder g -> Decoder result
+map7 f decoderA decoderB decoderC decoderD decoderE decoderF decoderG =
+    map4 f decoderA decoderB decoderC decoderD
+        |> andThen (\g -> map3 g decoderE decoderF decoderG)
+
+
+map8 : (a -> b -> c -> d -> e -> f -> g -> h -> result) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f -> Decoder g -> Decoder h -> Decoder result
+map8 f decoderA decoderB decoderC decoderD decoderE decoderF decoderG decoderH =
+    map4 f decoderA decoderB decoderC decoderD
+        |> andThen (\g -> map4 g decoderE decoderF decoderG decoderH)
+
+
+hardcoded : a -> Decoder (a -> b) -> Decoder b
+hardcoded =
+    map << (|>)
