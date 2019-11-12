dc11edb9f614a619b4659c9d7386524c561a2381
Folkert <folkert@folkertdev.nl>
2019-11-13 00:26:36 +0100

    Implement mapX using only map (and no andThen)
    The use of applicative (mapX) is generally prefered over monad (andThen)
    because andThen creates a closure. I'm not sure how relevant this
    currently is in elm, but still seems good practice to only use the maps.
    
    Also added map16, which can be useful when decoding big chunks

--- src/Bytes/Decode/Extra.elm
+++ src/Bytes/Decode/Extra.elm
@@ -1,7 +1,7 @@
 module Bytes.Decode.Extra exposing
     ( list, byteValues
     , andMap, hardcoded
-    , map6, map7, map8
+    , map6, map7, map8, map16
     , onlyOks, onlyJusts
     )
 
@@ -57,7 +57,7 @@ makes an equivalent to `Json.Decode.Pipeline.optional` difficult to impossible.
 
 ## Extra maps
 
-@docs map6, map7, map8
+@docs map6, map7, map8, map16
 
 
 ## Working with Results and Maybes
@@ -146,23 +146,78 @@ andMap aDecoder fnDecoder =
 
 {-| -}
 map6 : (a -> b -> c -> d -> e -> f -> result) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f -> Decoder result
-map6 f decoderA decoderB decoderC decoderD decoderE decoderF =
-    map5 f decoderA decoderB decoderC decoderD decoderE
-        |> andMap decoderF
+map6 f b1 b2 b3 b4 b5 b6 =
+    let
+        d1 =
+            Bytes.Decode.map4 (\a b c d -> f a b c d) b1 b2 b3 b4
+
+        d2 =
+            Bytes.Decode.map3 (\h a b -> h a b) d1 b5 b6
+    in
+    d2
 
 
 {-| -}
 map7 : (a -> b -> c -> d -> e -> f -> g -> result) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f -> Decoder g -> Decoder result
-map7 f decoderA decoderB decoderC decoderD decoderE decoderF decoderG =
-    map4 f decoderA decoderB decoderC decoderD
-        |> andThen (\g -> map3 g decoderE decoderF decoderG)
+map7 f b1 b2 b3 b4 b5 b6 b7 =
+    let
+        d1 =
+            Bytes.Decode.map4 (\a b c d -> f a b c d) b1 b2 b3 b4
+
+        d2 =
+            Bytes.Decode.map4 (\h a b c -> h a b c) d1 b5 b6 b7
+    in
+    d2
 
 
 {-| -}
 map8 : (a -> b -> c -> d -> e -> f -> g -> h -> result) -> Decoder a -> Decoder b -> Decoder c -> Decoder d -> Decoder e -> Decoder f -> Decoder g -> Decoder h -> Decoder result
-map8 f decoderA decoderB decoderC decoderD decoderE decoderF decoderG decoderH =
-    map4 f decoderA decoderB decoderC decoderD
-        |> andThen (\g -> map4 g decoderE decoderF decoderG decoderH)
+map8 f b1 b2 b3 b4 b5 b6 b7 b8 =
+    let
+        d1 =
+            Bytes.Decode.map4 (\a b c d -> f a b c d) b1 b2 b3 b4
+
+        d2 =
+            Bytes.Decode.map5 (\h a b c d -> h a b c d) d1 b5 b6 b7 b8
+    in
+    d2
+
+
+{-| -}
+map16 :
+    (b1 -> b2 -> b3 -> b4 -> b5 -> b6 -> b7 -> b8 -> b9 -> b10 -> b11 -> b12 -> b13 -> b14 -> b15 -> b16 -> result)
+    -> Decoder b1
+    -> Decoder b2
+    -> Decoder b3
+    -> Decoder b4
+    -> Decoder b5
+    -> Decoder b6
+    -> Decoder b7
+    -> Decoder b8
+    -> Decoder b9
+    -> Decoder b10
+    -> Decoder b11
+    -> Decoder b12
+    -> Decoder b13
+    -> Decoder b14
+    -> Decoder b15
+    -> Decoder b16
+    -> Decoder result
+map16 f b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15 b16 =
+    let
+        d1 =
+            Bytes.Decode.map4 (\a b c d -> f a b c d) b1 b2 b3 b4
+
+        d2 =
+            Bytes.Decode.map5 (\h a b c d -> h a b c d) d1 b5 b6 b7 b8
+
+        d3 =
+            Bytes.Decode.map5 (\h a b c d -> h a b c d) d2 b9 b10 b11 b12
+
+        d4 =
+            Bytes.Decode.map5 (\h a b c d -> h a b c d) d3 b13 b14 b15 b16
+    in
+    d4
 
 
 {-| A neat way to fill in predetermined information that's not part of the data
